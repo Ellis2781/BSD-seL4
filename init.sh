dab9f0e7655bddfaa9c5ab292f0a46462c4b8cf3
Ellis2781 <ellisstyles45@gmail.com>
2022-02-17 16:15:46 -0500

    Update

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-git submodule update --remote --merge
-ln -s tools/cmake-tool/init-build.sh init-build.sh
-ln -s tools/cmake-tool/default-CMakeLists.txt CMakeLists.txt
-ln -s tools/cmake-tool/init-build.sh init-build.sh
+git submodule init
+git submodule update
+#ln -s tools/cmake-tool/init-build.sh init-build.sh
+#ln -s tools/cmake-tool/default-CMakeLists.txt CMakeLists.txt
+#ln -s tools/cmake-tool/init-build.sh init-build.sh
 cd usr.bin/make && ./bootstrap
