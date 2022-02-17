ec87047d30890e5dd4d1f318778b90ca338ba422
Ellis2781 <ellisstyles45@gmail.com>
2022-02-17 15:56:25 -0500

    Update

--- settings.cmake
+++ settings.cmake
@@ -4,4 +4,3 @@ set(KernelArch x86 CACHE STRING "" FORCE)
 set(KernelSel4Arch ${PLATFORM} CACHE STRING "" FORCE)
 set(KernelPlatform pc99 CACHE STRING "" FORCE)
 
-set(musllibc_DIR ${CMAKE_SOURCE_DIR}/libc)
