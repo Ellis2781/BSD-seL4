c7af73b62b57907bbb7f5ff5990ab0dc2381a182
Ellis2781 <ellisstyles45@gmail.com>
2022-02-17 16:34:09 -0500

    Update

--- settings.cmake
+++ settings.cmake
@@ -4,3 +4,6 @@ set(KernelArch x86 CACHE STRING "" FORCE)
 set(KernelSel4Arch ${PLATFORM} CACHE STRING "" FORCE)
 set(KernelPlatform pc99 CACHE STRING "" FORCE)
 
+include(kernel/tools/helpers.cmake)
+
+set(GenerateSimulateScript true)
